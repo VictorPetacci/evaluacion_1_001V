f81cc1d9a4fc127c283f2ad06e5b7bc58be539b3
VictorPetacci <vi.petacci@duocuc.cl>
2026-09-09 20:58:39 -0300

    EVALUACION

---
+++ Reserva.kt
@@ -0,0 +1,10 @@
+
+open class Reserva(
+    val id: Int,
+    val cliente: String,
+    val noches: Int
+) {
+    open fun obtenerDescripcion(): String {
+        return "Reserva General ID #$id - Cliente: $cliente - Noches: $noches"
+    }
+}
---
+++ ResultadoRegistro.kt
@@ -0,0 +1,5 @@
+
+sealed class ResultadoRegistro {
+    data class Exito(val mensaje: String, val reserva: ReservaAlojamiento) : ResultadoRegistro()
+    data class Error(val motivo: String) : ResultadoRegistro()
+}
