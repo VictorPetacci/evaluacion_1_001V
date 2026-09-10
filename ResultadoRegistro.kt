6cd52fb7507b3f249a3fced87efed550bff0048b
VictorPetacci <vi.petacci@duocuc.cl>
2026-09-09 21:24:01 -0300

    Delete ResultadoRegistro.kt

--- ResultadoRegistro.kt
+++
@@ -1,5 +0,0 @@
-
-sealed class ResultadoRegistro {
-    data class Exito(val mensaje: String, val reserva: ReservaAlojamiento) : ResultadoRegistro()
-    data class Error(val motivo: String) : ResultadoRegistro()
-}
