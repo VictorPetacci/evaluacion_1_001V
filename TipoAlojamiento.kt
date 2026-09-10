5409f03688c892b0b16dea4b51fd76dab3b0fb46
VictorPetacci <vi.petacci@duocuc.cl>
2026-09-09 21:24:11 -0300

    Delete TipoAlojamiento.kt

--- TipoAlojamiento.kt
+++
@@ -1,6 +0,0 @@
-
-sealed class TipoAlojamiento(val descripcion: String) {
-    object Habitacion : TipoAlojamiento("Habitación")
-    object Cabana : TipoAlojamiento("Cabaña")
-    object Departamento : TipoAlojamiento("Departamento")
-}
